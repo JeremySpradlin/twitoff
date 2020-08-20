21184c8eaec65eb21e72a0333a91bc372ee1c2ba
JeremySpradlin <spradlinjk@gmail.com>
2020-08-20 12:39:14 -0400

    Getingi ready to deploy to heroku!

--- twitoff/models.py
+++ twitoff/models.py
@@ -26,34 +26,3 @@ class Tweet(DB.Model):
 
     def __repr__(self):
         return '-Tweet {}-'.format(self.text)
-
-
-# Commented out old methods for new api usage
-# Equivlanet methods are now located in twitter.py
-# def insert_example_users():
-#     """Example data to play with."""
-#     austen = User(id=1, name='austen')
-#     elon = User(id=2, name='elonmusk')
-#     jeremy = User(id=3, name='JeremySpradlin')
-#
-#     DB.session.add(austen)
-#     DB.session.add(elon)
-#     DB.session.add(jeremy)
-#     DB.session.commit()
-#
-#
-# def insert_example_tweets():
-#     """ Example tweets to play with"""
-#     t1 = Tweet(id=1, text='This is a tweet', user_id=3)
-#     t2 = Tweet(id=2, text='This is another tweet!', user_id=2)
-#     t3 = Tweet(id=3, text='THESE ARE ALL TWEETS!!!', user_id=1)
-#     t4 = Tweet(id=4, text='MOAR TWEETS!!!!', user_id=3)
-#     t5 = Tweet(id=5, text='Dr Rollergator For Prez!', user_id=3)
-#     t6 = Tweet(id=6, text='Im just Elon Musk with Tusks', user_id=2)
-#     DB.session.add(t1)
-#     DB.session.add(t2)
-#     DB.session.add(t3)
-#     DB.session.add(t4)
-#     DB.session.add(t5)
-#     DB.session.add(t6)
-#     DB.session.commit()
